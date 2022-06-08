a2ddcb0e10be743bf2c72f717266af4275842378
TimWoosh <tim.wostemeier@gmail.com>
2022-06-08 20:36:04 +0200

    fixed bug

--- Player.gd
+++ Player.gd
@@ -30,8 +30,12 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
+	
+	
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
@@ -45,10 +49,12 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 		
-		position += velocity * delta
-		position.x = clamp(position.x, 0, screen_size.x)
-		position.y = clamp(position.y, 0, screen_size.y)
-
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+		
+		
+		
 
 func _on_Player_body_entered(body):
 	hide() # Player disappears after being hit.
